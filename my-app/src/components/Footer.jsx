be405b4e2cff657974b3f6a5824fbb9ec32b35a0
BrenoBar51 <brenobarcelos9@gmail.com.com>
2023-06-28 09:43:18 -0300

    ok

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer>
+            <h1>Manel Gomi morreu com 10 tiro nas costas</h1>
+        </footer>
+    )
+}
---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,27 @@
+//function Header() {
+//
+//}
+//
+//export default Header
+
+//export default function Header() {
+//    return(
+//        <>
+//        </>
+//    )
+//}
+
+//export const Header = () => {
+//    return(
+//        <>
+//        </>
+//    )
+//}
+
+export default function Header() {
+    return (
+        <header>
+            <h1>E é bom dimaize</h1>
+        </header>
+    )
+}
